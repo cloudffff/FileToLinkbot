5f6d33229571d80a3f9774113136f8688d1e57ac
cloudffff <138120377+cloudffff@users.noreply.github.com>
2024-04-19 19:52:34 +0330

    Update remove.py

--- remove.py
+++ remove.py
@@ -6,11 +6,11 @@ current_time = time.time()
 directory = '/var/www/html'
 
 #add cron job for this file - cronatb -e in linux
-#files in directory will delete after 90 min automatically
+#files in directory will delete after 30 min automatically
 
 for root,dirs,files in os.walk(directory):
      for file_name in files:
           path = '%s/%s'%(root,file_name)
           creation_time = os.path.getctime(path)
-          if current_time - creation_time > (90*60):
+          if current_time - creation_time > (30*60):
               os.remove(path)
